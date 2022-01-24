6ab7e75b5a8576c45d2100ceacdce4e0cae6a31e
yuki <10756044@ntub.edu.tw>
2022-01-24 11:25:45 +0800

    update settings

--- core/settings.py
+++ core/settings.py
@@ -1,8 +1,11 @@
 import dj_database_url
+
 INSTALLED_APPS = [
     'rest_framework',
     'api.apps.ApiConfig',
 ]
+
+
 DATABASES = {
     'default': dj_database_url.parse('postgres://sxuszgkmgftinq:26e93186ad793b64a6ddb430f507d032f6d564b99412001a6c02d7a82268ffe0@ec2-44-199-52-133.compute-1.amazonaws.com:5432/ddkgsilnuogmu2')
 }
